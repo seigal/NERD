f7c4d8a5151ef5035411a97a75791eb7cbf02c31
seigal <annaseigal@airbears2-10-142-83-17.airbears2.1918.berkeley.edu>
2016-06-17 16:16:53 -0700

    Latest version of the function

--- R/NERD_function.R
+++ R/NERD_function.R
@@ -4,23 +4,26 @@
 #' @param
 #' times The date information. Should be stored in non-negative integers (e.g. days).
 #' @param
-#' res_info The resistance information. Should be stored on a discrete scale from 1 to some value m. Values below 0 are discarded. For us: load "CData" which stores evolution information and select appropriate column to extract antibiotic of interest.
+#' res_info The resistance information. Should be stored on a discrete scale from 1 to some value m. Values below 0 are discarded.
 #' @param
-#' param The parameter for the Poisson part of the test. Defaults to 365 days = 1 year.
+#' mu The mean for the negative binomial distribution which models the outbreak distribution of the hospital in question. Estimated according to the size of the hospital. Defaults to 100 days.
+#' @param
+#' size The parameter for the size/over-dispersion of the negative binomial distribution. Defaults to the value 10.
+#' @param
+#' final How many samples to generate for the permutation test. Defaults to FALSE, indicating that 1000 samples should be used. TRUE ups the precision to 10,000 samples (but should be run with patience).
 #' @keywords Antibiotic Resistance
 #' @export
 #' @examples
 #' NERD_function()
 
-NERD_function <- function(times,res_info,param=365) { 
-	
+NERD_function <- function(times,res_info,mu=100,size=10,final=FALSE) { 
+
+	# doing checks
 	N <- length(times);
-	NN <- length(res_info);
-	
+	NN <- length(res_info);	
 	if (N != NN) {
 		stop("Time information and resistance information have the same length")
-	}
-	
+	}	
 	m <- max(res_info);
 	m_counts <- rep(0,m);
 	for (i in 1:m) {
@@ -30,26 +33,23 @@ NERD_function <- function(times,res_info,param=365) {
 	if (min_m < 1) {
 		stop("Must be a postive number of counts in each resistance category")
 	}
-		
 	
-	
-
-	
+	# formatting the data
 	times <- times[res_info > 0];
 	res_info <- res_info[res_info > 0];
 	ptimes1 <- array( rep(times, N), c(N,N));
 	ptimes2 <- t(ptimes1);
 	ptimes <- ptimes1 - ptimes2;
 	ptimes[ptimes < 0] <- 0;
-	#m = max(res_info);
 	
+	# generating the chi-squared value for the data
 	Markov = array( 0,c(m,m))
 	for (i in 1:m) {
 		for (j in 1:m) {
 			cols <- (res_info == j);
 			rows <- (res_info == i);
-			pt <- ptimes[rows,cols]; # raw pairwise time differences
-			ptw <- dpois(pt, param, log = FALSE); # weighted by Poisson
+			pt <- ptimes[rows,cols]; # pairwise time differences
+			ptw <- dnbinom(pt, mu = mu,size = size); # negbin weight
 			Markov[j,i] <- sum(ptw);
 		}
 	}
@@ -70,14 +70,13 @@ NERD_function <- function(times,res_info,param=365) {
 	chi_data <- sum(chi)
 
 
-
-
-
-
-
-	# then we run simulations to compare
-
-	M <- 1000; # the number of randomized strings we compare to
+	# running simulations to compare
+	if (final == TRUE) {
+		M <- 10000
+		}
+		else {
+		M <- 1000;
+	}
 	chis <- rep(0, M);
 	for (jj in 1:M) {
 		
@@ -96,7 +95,7 @@ NERD_function <- function(times,res_info,param=365) {
 				cols <- (res_info == j);
 				rows <- (res_info == i);
 				pt <- ptimes[rows,cols];
-				ptw <- dpois(pt, param, log = FALSE)
+				ptw <- dnbinom(pt, size=size,mu=mu);
 				Markov[j,i] <- sum(ptw);
 			}
 		}
@@ -121,7 +120,6 @@ NERD_function <- function(times,res_info,param=365) {
 
 	chis1 <- sort(chis);
 	pvalue <- sum(chis > chi_data)/M;
-
 	markov_data;
 	Counts;
 	pvalue;
